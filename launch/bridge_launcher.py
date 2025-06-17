d65876749924e9e690b5603bad739e1189d58a93
Clemente Donoso <clemente.donosok@gmail.com>
2025-06-17 16:32:02 +0200

    Add livox viewer

---
+++ launch/bridge_launcher.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Nodo que lee y publica JointStates
+        Node(
+            package='inria_unitree',
+            executable='ros_bridge',
+            name='ros_bridge',
+            parameters=[{'interface': 'eth0'}],
+            output='screen'
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='mid360_to_livox_tf',
+            arguments=[
+                '0', '0', '0',
+                '0', '0', '3.14159265',
+                'mid360_link',
+                'livox_frame'
+            ]
+        )
+    ])
